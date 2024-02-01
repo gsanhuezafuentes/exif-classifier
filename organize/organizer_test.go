13c1883b7006c0e8b4fdbd135f0af26208978c68
Gabriel Sanhueza Fuentes <gsanhuezafuentes11@gmail.com>
2024-02-01 19:34:17 -0300

    Change the way of test date are created in organizer_test

--- organize/organizer_test.go
+++ organize/organizer_test.go
@@ -9,6 +9,22 @@ import (
 	"time"
 )
 
+var testFileNames = []string{"file1.dng", "file2.jpg", "file3.arw", "file4.arw"}
+var creationTime = []string{"2024-02-01", "2024-02-02", "2024-02-02", "2024-02-03"}
+var orientation = []string{"Horizontal", "Horizontal", "Vertical", "Unknown"}
+var lens = []string{"Sony 55-210", "Sigma 16mm", "Tamrom 150-500mm", "Tamrom 150-500mm"}
+
+func createTestFiles(t *testing.T, tempDir string, filepaths []string) (files []string) {
+	for _, file := range filepaths {
+		fullpath := filepath.Join(tempDir, file)
+		newFile, err := os.Create(fullpath)
+		newFile.Close()
+		files = append(files, fullpath)
+		assert.NoError(t, err)
+	}
+	return
+}
+
 type mockExifReader struct {
 	MockResult map[string]*er.ExifData
 }
@@ -22,20 +38,14 @@ func TestDefaultOrganizer_OrganizeImgsByDate(t *testing.T) {
 	defer os.RemoveAll(tempDir)
 	assert.NoError(t, err)
 
-	var files []string
-	for _, file := range []string{"file1.dng", "file2.jpg", "file3.arw"} {
-		fullpath := filepath.Join(tempDir, file)
-		newFile, err := os.Create(fullpath)
-		newFile.Close()
-		files = append(files, fullpath)
-		assert.NoError(t, err)
-	}
+	files := createTestFiles(t, tempDir, testFileNames)
 
 	var mockValues = make(map[string]*er.ExifData)
-	start := time.Date(2024, 2, 1, 18, 11, 0, 0, time.UTC)
 	for i, file := range files {
+		date, err := time.Parse(DATE_FORMAT, creationTime[i])
+		assert.NoError(t, err)
 		mockValues[file] = &er.ExifData{
-			CreatedTime: start.Add(time.Duration(i * int(24*time.Hour))),
+			CreatedTime: date,
 		}
 	}
 
@@ -62,17 +72,9 @@ func TestDefaultOrganizer_OrganizeImgsByOrientation(t *testing.T) {
 	defer os.RemoveAll(tempDir)
 	assert.NoError(t, err)
 
-	var files []string
-	for _, file := range []string{"file1.dng", "file2.jpg", "file3.arw"} {
-		fullpath := filepath.Join(tempDir, file)
-		newFile, err := os.Create(fullpath)
-		newFile.Close()
-		files = append(files, fullpath)
-		assert.NoError(t, err)
-	}
+	files := createTestFiles(t, tempDir, testFileNames)
 
 	var mockValues = make(map[string]*er.ExifData)
-	orientation := []string{"Horizontal", "Vertical", "Unknown"}
 	for i, file := range files {
 		mockValues[file] = &er.ExifData{
 			Orientation: orientation[i],
@@ -102,17 +104,9 @@ func TestDefaultOrganizer_OrganizeImgsByLens(t *testing.T) {
 	defer os.RemoveAll(tempDir)
 	assert.NoError(t, err)
 
-	var files []string
-	for _, file := range []string{"file1.dng", "file2.jpg", "file3.arw"} {
-		fullpath := filepath.Join(tempDir, file)
-		newFile, err := os.Create(fullpath)
-		newFile.Close()
-		files = append(files, fullpath)
-		assert.NoError(t, err)
-	}
+	files := createTestFiles(t, tempDir, testFileNames)
 
 	var mockValues = make(map[string]*er.ExifData)
-	lens := []string{"Sony 55-210", "Sigma 16mm", "Tamrom 150-500mm"}
 	for i, file := range files {
 		mockValues[file] = &er.ExifData{
 			LensModel: lens[i],
@@ -131,6 +125,44 @@ func TestDefaultOrganizer_OrganizeImgsByLens(t *testing.T) {
 	for key, exifData := range mockValues {
 		folder := exifData.LensModel
 		_, err := os.Stat(filepath.Join(filepath.Dir(key), folder, filepath.Base(key)))
+		if os.IsNotExist(err) {
+			t.Fatal("File was not classified.")
+		}
+	}
+}
+
+func TestDefaultOrganizer_All(t *testing.T) {
+	tempDir, err := os.MkdirTemp("", "test_organizer")
+	defer os.RemoveAll(tempDir)
+	assert.NoError(t, err)
+
+	files := createTestFiles(t, tempDir, testFileNames)
+
+	var mockValues = make(map[string]*er.ExifData)
+	for i, file := range files {
+		date, err := time.Parse(DATE_FORMAT, creationTime[i])
+		assert.NoError(t, err)
+		mockValues[file] = &er.ExifData{
+			CreatedTime: date,
+			Orientation: orientation[i],
+			LensModel:   lens[i],
+		}
+	}
+
+	exifReader := &mockExifReader{
+		MockResult: mockValues,
+	}
+	organizer := NewDefaultOrganizer(exifReader, os.Rename)
+
+	organizer.SetImagesPath(files)
+
+	assert.NoError(t, organizer.OrganizeImgsByDate())
+	assert.NoError(t, organizer.OrganizeImgsByOrientation())
+	assert.NoError(t, organizer.OrganizeImgsByLens())
+
+	for key, exifData := range mockValues {
+		date := exifData.CreatedTime.Format(DATE_FORMAT)
+		_, err := os.Stat(filepath.Join(filepath.Dir(key), date, exifData.Orientation, exifData.LensModel, filepath.Base(key)))
 		if os.IsNotExist(err) {
 			t.Fatal("File was not classified.")
 		}
